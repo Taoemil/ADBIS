d8bd4a1ea64237e6342296e36b68e35b9741365a
Taoemil <taoemil@hotmail.com>
2023-05-22 13:16:19 +0200

    pdf uploaded

--- database/createTable.js
+++ database/createTable.js
@@ -1,6 +1,34 @@
+const fs = require('fs');
 const db = require('./db.js');
 
-// Create a table for storing PDF files
+// Function to store a PDF file in the database
+function storePDF(title, subject, keywords, createdDate, deletedDate, pageNumbers, filePath) {
+  const data = fs.readFileSync(filePath);
+
+  const sql = `INSERT INTO pdfs (title, subject, keywords, createdDate, deletedDate, pageNumbers, data)
+               VALUES (?, ?, ?, ?, ?, ?, ?)`;
+
+  db.run(sql, [title, subject, keywords, createdDate, deletedDate, pageNumbers, data], function (error) {
+    if (error) {
+      console.error('Failed to store the PDF:', error);
+    } else {
+      console.log('PDF stored successfully!');
+    }
+  });
+}
+
+// Example usage
+const pdfTitle = 'Oversigt';
+const pdfSubject = 'emneEksempel';
+const pdfKeywords = 'soegeord';
+const pdfCreatedDate = '2023-05-22';
+const pdfDeletedDate = null; // Set to null if the PDF is not deleted
+const pdfPageNumbers = 5;
+const pdfFilePath = '/Users/emil/Downloads/vejledningsoversigt.pdf';
+
+storePDF(pdfTitle, pdfSubject, pdfKeywords, pdfCreatedDate, pdfDeletedDate, pdfPageNumbers, pdfFilePath);
+
+/* Create a table for storing PDF files
 db.run(`
   CREATE TABLE IF NOT EXISTS pdfs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,3 +47,4 @@ db.run(`
     console.log('PDF table created successfully!');
   }
 });
+*/
